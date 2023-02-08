3cb45984e36dd4f5b03fe5fbbb8c2468ee5cb033
Shivam-Cogoport <shivam.rai@github.com>
2023-02-08 19:58:46 +0530

    Ruby On Rails -- Backend

---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+
+  #User
+  post "/user/create", to: "user#create"
+  get "/users", to: "user#readAllUser"
+  get "/user/:id", to: "user#readUser"
+  get "/user/article/:id", to: "user#readUserArticles"
+  delete "/user/delete/:id", to: "user#delete"
+
+  #Article
+  post "/article/create", to: "article#create"
+  get "/articles", to: "article#readAll"
+  get "/article/title/:title", to: "article#readByTitle"
+  get "/article/page/:pageNo", to: "article#readPage"
+  get "/article/range", to: "article#readByRange"
+  get "article/:id", to: "article#readUserArticles"
+  get "/article/category/:category", to: "article#readByCategory"
+  put "/article/update/:id", to: "article#update"
+  delete "/article/delete/:id", to: "article#delete"
+  delete "/article/delete_all", to: "article#deleteAll"
+
+  #Category
+  post "/category/create", to: "category#create"
+  get "/categories", to: "category#readAllCategory"
+  get "/category/:category_id", to: "category#findByCategory"
+  
+  #ArticleCategory
+  get "/articlecategory", to: "article_category#readAll"
+
+end
